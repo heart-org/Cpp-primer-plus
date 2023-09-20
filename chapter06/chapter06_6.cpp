7ff052699d7d5bde169f53f93f3cba127d36461f
orange <org159753@163.com>
2023-09-20 18:30:01 +0800

    init commit

---
+++ chapter06/chapter06_6.cpp
@@ -0,0 +1,43 @@
+#include "iostream"
+#include "queue"
+#include "stack"
+#include "vector"
+#include "algorithm"
+#include "cstring"
+#include "cmath"
+#include "set"
+#include "map"
+#include "numeric"
+#include "unordered_set"
+#include "unordered_map"
+#include "array"
+
+using namespace std;
+
+int main(){
+    long long wage;
+    int tax = 0;
+    cout << "Please enter your wages: " << endl;
+    cin >> wage;
+    while (wage){
+        if (wage <= 5000)
+            cout << " No Tax " << endl;
+        else if ((wage > 5000) && (wage <= 15000)){
+            tax = (wage - 5000) * 0.1;
+            cout << tax << endl;
+        }
+        else if ((wage > 15000) && (wage <= 35000)){
+            tax = 1000 + (wage - 15000) * 0.15;
+            cout << tax << endl;
+        }else{
+
+        }
+
+
+
+
+    }
+
+
+    return 0;
+}
